949a5fcc47bf0bd268dac0f8a1af97e091b7af26
jiwon <kjo6152@naver.com>
2014-07-26 23:32:29 +0900

    InputHandler init commit

--- phonetop/src/org/secmem232/phonetop/android/MouseMappingActivity.java
+++ phonetop/src/org/secmem232/phonetop/android/MouseMappingActivity.java
@@ -1,6 +1,7 @@
 package org.secmem232.phonetop.android;
 
 import org.secmem232.phonetop.R;
+import org.secmem232.phonetop.android.service.PhonetopInputHandler;
 import org.secmem232.phonetop.android.service.PhonetopService;
 import org.secmem232.phonetop.android.service.PhonetopServiceBinder;
 import org.secmem232.phonetop.android.util.Util;
@@ -19,11 +20,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
+/**
+ * 각 마우스의 버튼이 눌렸을 때 핸드폰에 적용될 행동을 설정 할 수 있는 Activity.
+ * @author youngsick
+ *
+ */
 public class MouseMappingActivity extends Activity {
 	private ViewGroup leftButtonLayout;
 	private ViewGroup rightButtonLayout;
 	private ViewGroup wheelButtonLayout;
 
+	/**
+	 * 서비스에 연결되었을 때 미리 저장된 key 와 value를 가지고 실제 서비스의 마우스 액션을 설정하는 기능을 가지는 Connection객체
+	 */
 	private ServiceConnection mappingServiceConnection;
 
 	int key;
@@ -42,6 +51,7 @@ public class MouseMappingActivity extends Activity {
 			@Override
 			public void onServiceConnected(ComponentName name, IBinder service) {
 				// TODO Auto-generated method stub
+				//바인더를 통해 실제 서비스에 맵핑적용 key:버튼, value:액션
 				((PhonetopServiceBinder) service).setMouseMapping(key,value);
 				unbindService(this);
 			}
@@ -54,50 +64,32 @@ public class MouseMappingActivity extends Activity {
 
 		leftButtonLayout = (ViewGroup) findViewById(R.id.btn_left);
 		leftButtonLayout.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				// TODO Auto-generated method stub
-				btnLeftPosition = Util.getIntegerPreferences(
-						MouseMappingActivity.this, "btn_left");
+				btnLeftPosition = Util.getIntegerPreferences(MouseMappingActivity.this, "btn_left");
 				final String items[] = { "touch(Click)", "back", "home", "menu" };
-				ContextThemeWrapper cw = new ContextThemeWrapper(
-						MouseMappingActivity.this, R.style.AlertDialogTheme);
+				ContextThemeWrapper cw = new ContextThemeWrapper(MouseMappingActivity.this, R.style.AlertDialogTheme);
 				AlertDialog.Builder ab = new AlertDialog.Builder(cw);
 				ab.setTitle("left button");
 				ab.setSingleChoiceItems(items, btnLeftPosition,
 						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,
-									int whichButton) {
-								// �� ����Ʈ�� ����������
-							}
+							public void onClick(DialogInterface dialog,int whichButton) {}
 						})
 						.setPositiveButton("Ok",
 								new DialogInterface.OnClickListener() {
 									public void onClick(DialogInterface dialog,
 											int whichButton) {
-										// OK ��ư Ŭ���� , ���⼭ ������ ���� ����
-										// Activity �� �ѱ�� �ȴ�.
-										key = PhonetopService.LEFT_BUTTON;
-										value = ((AlertDialog) dialog)
-												.getListView()
-												.getCheckedItemPosition();
-										Util.saveIntegerPreferences(
-												MouseMappingActivity.this,
-												"btn_left", value);
-										bindService(new Intent(
-												MouseMappingActivity.this,
-												PhonetopService.class),
-												mappingServiceConnection, 0);
+										key = PhonetopInputHandler.LEFT_BUTTON;
+										value = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+										Util.saveIntegerPreferences(MouseMappingActivity.this,"btn_left", value);
+										//Connection객체를 PhonetopService와 bind시켜 현재 액티비티에서 서비스의 작업을 조작함.
+										bindService(new Intent(MouseMappingActivity.this,PhonetopService.class),mappingServiceConnection, 0);
 									}
 								})
-						.setNegativeButton("Cancel",
+								.setNegativeButton("Cancel",
 								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int whichButton) {
-										// Cancel ��ư Ŭ����
-									}
+									public void onClick(DialogInterface dialog,int whichButton) {		}
 								});
 				ab.show();
 			}
@@ -109,46 +101,28 @@ public class MouseMappingActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				// TODO Auto-generated method stub
-				btnRightPosition = Util.getIntegerPreferences(
-						MouseMappingActivity.this, "btn_right");
+				btnRightPosition = Util.getIntegerPreferences(MouseMappingActivity.this, "btn_right");
 				final String items[] = { "touch(Click)", "back", "home", "menu" };
-				ContextThemeWrapper cw = new ContextThemeWrapper(
-						MouseMappingActivity.this, R.style.AlertDialogTheme);
+				ContextThemeWrapper cw = new ContextThemeWrapper(MouseMappingActivity.this, R.style.AlertDialogTheme);
 				AlertDialog.Builder ab = new AlertDialog.Builder(cw);
 				ab.setTitle("right button");
 				ab.setSingleChoiceItems(items, btnRightPosition,
 						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,
-									int whichButton) {
-								// �� ����Ʈ�� ����������
-							}
+							public void onClick(DialogInterface dialog,int whichButton) {	}
 						})
 						.setPositiveButton("Ok",
 								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int whichButton) {
-										// OK ��ư Ŭ���� , ���⼭ ������ ���� ����
-										// Activity �� �ѱ�� �ȴ�.
-										key = PhonetopService.RIGHT_BUTTON;
-										value = ((AlertDialog) dialog)
-												.getListView()
-												.getCheckedItemPosition();
-										Util.saveIntegerPreferences(
-												MouseMappingActivity.this,
-												"btn_right", value);
-										bindService(new Intent(
-												MouseMappingActivity.this,
-												PhonetopService.class),
-												mappingServiceConnection, 0);
+									public void onClick(DialogInterface dialog,	int whichButton) {
+										key = PhonetopInputHandler.RIGHT_BUTTON;
+										value = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+										Util.saveIntegerPreferences(MouseMappingActivity.this,"btn_right", value);
+										//Connection객체를 PhonetopService와 bind시켜 현재 액티비티에서 서비스의 작업을 조작함.
+										bindService(new Intent(MouseMappingActivity.this,PhonetopService.class),mappingServiceConnection, 0);
 									}
 								})
 						.setNegativeButton("Cancel",
 								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int whichButton) {
-										// Cancel ��ư Ŭ����
-									}
+									public void onClick(DialogInterface dialog,int whichButton) {		}
 								});
 				ab.show();
 			}
@@ -160,46 +134,28 @@ public class MouseMappingActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				// TODO Auto-generated method stub
-				btnWheelPosition = Util.getIntegerPreferences(
-						MouseMappingActivity.this, "btn_wheel");
+				btnWheelPosition = Util.getIntegerPreferences(MouseMappingActivity.this, "btn_wheel");
 				final String items[] = { "touch(Click)", "back", "home", "menu" };
-				ContextThemeWrapper cw = new ContextThemeWrapper(
-						MouseMappingActivity.this, R.style.AlertDialogTheme);
+				ContextThemeWrapper cw = new ContextThemeWrapper(MouseMappingActivity.this, R.style.AlertDialogTheme);
 				AlertDialog.Builder ab = new AlertDialog.Builder(cw);
 				ab.setTitle("wheel button");
 				ab.setSingleChoiceItems(items, btnWheelPosition,
 						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,
-									int whichButton) {
-								// �� ����Ʈ�� ����������
-							}
+							public void onClick(DialogInterface dialog,int whichButton) {}
 						})
 						.setPositiveButton("Ok",
 								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int whichButton) {
-										// OK ��ư Ŭ���� , ���⼭ ������ ���� ����
-										// Activity �� �ѱ�� �ȴ�.
-										key = PhonetopService.WHEEL_BUTTON;
-										value = ((AlertDialog) dialog)
-												.getListView()
-												.getCheckedItemPosition();
-										Util.saveIntegerPreferences(
-												MouseMappingActivity.this,
-												"btn_wheel", value);
-										bindService(new Intent(
-												MouseMappingActivity.this,
-												PhonetopService.class),
-												mappingServiceConnection, 0);
+									public void onClick(DialogInterface dialog,int whichButton) {
+										key = PhonetopInputHandler.WHEEL_BUTTON;
+										value = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+										Util.saveIntegerPreferences(MouseMappingActivity.this,"btn_wheel", value);
+										//Connection객체를 PhonetopService와 bind시켜 현재 액티비티에서 서비스의 작업을 조작함.
+										bindService(new Intent(MouseMappingActivity.this,PhonetopService.class),mappingServiceConnection, 0);
 									}
 								})
 						.setNegativeButton("Cancel",
 								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int whichButton) {
-										// Cancel ��ư Ŭ����
-									}
+									public void onClick(DialogInterface dialog,int whichButton) {}
 								});
 				ab.show();
 			}
--- phonetop/src/org/secmem232/phonetop/android/service/PhonetopService.java
+++ phonetop/src/org/secmem232/phonetop/android/service/PhonetopService.java
@@ -1,17 +1,16 @@
 package org.secmem232.phonetop.android.service;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
 
 import org.secmem232.phonetop.android.MainActivity;
 import org.secmem232.phonetop.android.MouseView;
 import org.secmem232.phonetop.android.UIHandler;
-import org.secmem232.phonetop.android.natives.InputHandler;
 import org.secmem232.phonetop.android.util.Util;
+
+import com.android.phonetop.PhonetopDisplayManager;
+import com.android.phonetop.PhonetopTetheringManager;
 
 import android.app.Service;
 import android.content.Intent;
@@ -23,89 +22,31 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.android.phonetop.*;
-
 public class PhonetopService extends Service {
 	static String tag = "PhonetopService";
+	/**
+	 * 화면 세로/가로 상황에 따른 고유값 
+	 */
 	static final public int ORIENTATION_PORTRAIT = 0;
 	static final public int ORIENTATION_LANDSCAPE = 1;
-	
-	public static int CLOSE_DISPLAY_SERVICE = 0;
-	public static int SET_DISPLAY_ORIENTATION = 10;
-	public static int CHANGE_DISPLAY_ORIENTATION = 11;
-	
-	static final public byte  START_MOUSE_SERVICE = 11;
-	static final public byte  END_MOUSE_SERVICE = 12;
-	static final public byte  START_KEYBOARD_SERVICE = 13;
-	static final public byte  END_KEYBOARD_SERVICE = 14;
-	static final public byte  START_MONITOR_SERVICE = 15;
-	static final public byte  END_MONITOR_SERVICE = 16;
-	static final public byte  START_TETHERING_SERVICE = 17;
-	static final public byte  END_TETHERING_SERVICE = 18;
-	static final public byte  SET_MOUSE_WHEEL_VOLUEM = 19;
-	static final public byte  SET_MOUSE_SPEED = 20;
-	static final public byte  SET_MOUSE_MAPPING = 21;
-	static final public byte  SET_KEYBOARD_MAPPING = 22;
-	static final public byte  SET_MONITOR_ORIENTATION = 23;
-	
-	static final public byte NONE_DEVICE = 4;
-	static final public byte INPUT_KEYBOARD = 1;
-	static final public byte INPUT_MOUSE = 2;
-	static final public byte INPUT_ALLDEVICE = 3;
-	
-	static final public byte OUTPUT_MONITOR = 1;
-	static final public byte UTIL_THETHERING = 1;
-	
-	static final public int LEFT_BUTTON = 272;
-	static final public int RIGHT_BUTTON = 273;
-	static final public int WHEEL_BUTTON = 274;
 
-	static final public int WHEEL_SLOW = 0;
-	static final public int WHEEL_NORMAL = 1;
-	static final public int WHEEL_FAST = 2;
-
-	static final public int KEY_CLICK = 0;
-	static final public int KEY_BACK = 1;
-	static final public int KEY_HOME = 2;
-	static final public int KEY_MENU = 3;
-	
-	static final public int LEFT_HOME = 125;
-	static final public int RIGHT_HOME = 126;
-	static final public int HOME = 102;
-	static final public int VOLUME_DOWN = 114;
-	static final public int VOLUME_UP = 115;
-	static final public int KEY_F2 = 60;
-	static final public int KEY_F3 = 61;
-	static final public int KEY_F4 = 62;
-	static final public int KEY_POWER = 116;
-
+	//클라이언트 추가 유/무 판별 변수
 	int addedClient = 0;
+	//실제 마우스를 표현 할 최상위뷰
 	public MouseView view;
-	private boolean isEnd;
+
+	//서비스 종료시점을 판단해 줄 flag 변수 
+	//서비스관련 기능을 수행하는 Binder객체, PhonetopServiceConnection객체를 통해 MainActivity로부터 서비스를 관리. 
 	public PhonetopServiceBinder mPhonetopServiceBinder;
-	InputHandler inputHandler;
-	InputStream in;
-	OutputStream out;
-	byte a[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-			0, 0, 0 };
-	ByteBuffer buffer;
-	int type;
-	int code;
-	int value;
-	int inputMode;
+	//실제 이벤트 관련한 기능 수행 핸들러.
+	public PhonetopInputHandler inputEventHandler;
 
-	int btnLeft;
-	int btnRight;
-	int btnWheel;
-
-	int wheelSpeed;
-	
-	boolean viewAddFlag;
-	
-	DisplayRotation dr;
 	PhonetopDisplayManager mPhonetopDisplayManager;
 	PhonetopTetheringManager mPheontopTetheringManager;
 	
+	boolean viewAddFlag;
+
+	DisplayRotation dr;
 	private ServerSocket server;
 	private Socket client;
 	@Override
@@ -125,39 +66,18 @@ public class PhonetopService extends Service {
 		super.onCreate();
 		Log.i(tag, "service oncreate");
 
-		wheelSpeed = Util.getIntegerPreferences(this, "wheel");
-		btnLeft = Util.getIntegerPreferences(this, "btn_left");
-		btnRight = Util.getIntegerPreferences(this, "btn_right");
-		btnWheel = Util.getIntegerPreferences(this, "btn_wheel");
-
-		if (btnLeft < 0)
-			btnLeft = 0;
-		if (btnRight < 0)
-			btnRight = 1;
-		if (btnWheel < 0)
-			btnWheel = 2;
+		//마우스뷰 생성
 		view = new MouseView(this); // MainActivity.view=view;
-
+		//마우스뷰 최상위뷰로 등록
 		makeView();
-		
-		mPhonetopServiceBinder = new PhonetopServiceBinder(this);
-		mPhonetopServiceBinder.setMouseWheelVolume(Util.getIntegerPreferences(this, "wheel"));
-		mPhonetopServiceBinder.setMouseSpeed(Util.getIntegerPreferences(this, "speed"));
-		mPhonetopServiceBinder.setMouseMapping(LEFT_BUTTON, Util.getIntegerPreferences(this, "btn_left"));
-		mPhonetopServiceBinder.setMouseMapping(RIGHT_BUTTON, Util.getIntegerPreferences(this, "btn_right"));
-		mPhonetopServiceBinder.setMouseMapping(WHEEL_BUTTON, Util.getIntegerPreferences(this, "btn_wheel"));
-		mPhonetopServiceBinder.setMousePointerIcon(Util.getIntegerPreferences(this, "cursor"));
-		
-		// view.setOnTouchListener(mViewTouchListener); //팝업뷰에 터치 리스너 등록
-		
-		inputHandler = new InputHandler(this);
+
 		dr = new DisplayRotation(this);
+		mPhonetopServiceBinder = new PhonetopServiceBinder(this);
 		mPhonetopDisplayManager = new PhonetopDisplayManager(this);
 		mPheontopTetheringManager = new PhonetopTetheringManager(this);
 		//Usb Display Server Open
 		mPhonetopDisplayManager.connectUsbDisplay("0.0.0.0");
 		mPheontopTetheringManager.setUsbTethering(true);
-		
 		startInputServer();
 	}
 
@@ -173,6 +93,7 @@ public class PhonetopService extends Service {
 						addedClient++;
 						setAddedClient(addedClient);
 					}
+					inputEventHandler = new PhonetopInputHandler(PhonetopService.this,client,view);
 					runInputService();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -181,145 +102,7 @@ public class PhonetopService extends Service {
 			}
 		}.start();
 	}
-	public void executeMouseFunction(int num) {
-		Log.d("name", "num : " + num);
-		switch (num) {
-		case KEY_CLICK:
-			if (value == 1) {// 누름,이동
-				inputHandler.sendEvent(1, 330, 1);
-				inputHandler.sendEvent(0, 0, 0);
-			} else if (value == 0) {// 뗌
-				inputHandler.sendEvent(1, 330, 0);// BTN_TOUCH:330 Ox14a
-				inputHandler.sendEvent(0, 0, 0);
-			}
-			break;
-		case KEY_BACK:
-			if (value == 0) {// 뗌
-				inputHandler.sendEvent(1, 158, 1);// KEY_BACK:158
-				inputHandler.sendEvent(0, 0, 0);
-				inputHandler.sendEvent(1, 158, 0);// KEY_BACK:158
-				inputHandler.sendEvent(0, 0, 0);
-			}
-			break;
-		case KEY_HOME:
-			if (value == 1) {// 누름,이동
-				inputHandler.sendEvent(1, 102, 1);
-				inputHandler.sendEvent(0, 0, 0);
-			} else if (value == 0) {// 뗌
-				inputHandler.sendEvent(1, 102, 0);
-				inputHandler.sendEvent(0, 0, 0);
-			}
-			break;
-		case KEY_MENU:
-			if (value == 1) {// 누름,이동
-				inputHandler.sendEvent(1, 139, 1);
-				inputHandler.sendEvent(0, 0, 0);
-			} else if (value == 0) {// 뗌
-				inputHandler.sendEvent(1, 139, 0);
-				inputHandler.sendEvent(0, 0, 0);
-			}
-			break;
-		}
-	}
 
-	public void setEventType(boolean isMouse,boolean isKeyboard){
-		try {
-			if(!isMouse&&!isKeyboard)
-				out.write(NONE_DEVICE);
-			else if(isMouse&&isKeyboard)
-				out.write(INPUT_ALLDEVICE);
-			else if(isMouse)
-				out.write(INPUT_MOUSE);
-			else//(isKeyboard)
-				out.write(INPUT_KEYBOARD);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void buttonEvents() {
-		try {
-			//32bit용
-			int ret = in.read(a, 0, 16);
-			if(ret<=0){
-				isEnd = false;
-				return;
-			}
-			buffer.rewind();
-			buffer.put(a);
-			//32bit용
-			type = buffer.getShort(8);
-			code = buffer.getShort(10);
-			value = buffer.getInt(12);
-
-//			Log.d("TCP/IPtest", "type : " + type + ",code : " + code+ ",value : " + value);
-			if (view == null)
-				return;
-
-			switch (type) {
-			case 1:
-				if (code == LEFT_BUTTON) {// 마우스 좌버튼 클릭시
-					Log.d("name", "left : " + btnLeft);
-					executeMouseFunction(btnLeft);
-				} else if (code == RIGHT_BUTTON) {// 마우스 우버튼 클릭시
-					Log.d("name", "right : " + btnRight);
-					executeMouseFunction(btnRight);
-				} else if (code == WHEEL_BUTTON) {// 마우스 휠 버튼 클릭시
-					Log.d("name", "wheel : " + btnWheel);
-					executeMouseFunction(btnWheel);
-				} else if(code == LEFT_HOME || code == RIGHT_HOME){
-					inputHandler.sendEvent(type, HOME, value);
-				} else if(code == KEY_F2){
-					inputHandler.sendEvent(type, VOLUME_DOWN, value);
-				} else if(code == KEY_F3){
-					inputHandler.sendEvent(type, VOLUME_UP, value);
-				} else if(code == KEY_F4){
-					System.out.println("POWER: " + KEY_POWER);
-					inputHandler.sendEvent(type, KEY_POWER, value);
-				} else if(code == 41){
-					inputHandler.sendEvent(type,399, value);
-				} else {
-					inputHandler.sendEvent(type, code, value);
-					return;
-				}
-				break;
-			case 2:
-				// 움직일때 x,y갱신
-				if (code == 0) {// 좌우
-					view.setRelativeCurser_X(value);
-					view.postInvalidate();
-				} else if (code == 1) {// 상하
-					view.setRelativeCurser_Y(value);
-					view.postInvalidate();
-				} else if (code == 8) {
-					if (value > 0) {// 휠아래로
-						for(int i=0;i<=this.wheelSpeed;i++)
-						inputHandler.keyStroke(103);
-					} else if (value < 0) {// 휠위로
-						for(int i=0;i<=this.wheelSpeed;i++)
-						inputHandler.keyStroke(108);
-					}
-					//Util.makeToast(this, "Wheel : "+wheelSpeed, Toast.LENGTH_SHORT);
-					return;
-				}
-				break;
-			case 4:
-				if (value < 100)
-					inputHandler.sendEvent(type, code, value);
-				return;
-			default:
-				inputHandler.sendEvent(type, code, value);
-			}			
-			inputHandler.sendEvent(3, 0, view.getValueX());// ABS_X:0 EV_ABS:3
-			inputHandler.sendEvent(3, 1, view.getValueY());// ABS_Y:1
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// 계속 마우스 입력 파일을 읽어서 setCursor()갱신시켜주는 로직
-	}
 
 	private void runInputService() {
 		new Thread() {
@@ -328,25 +111,8 @@ public class PhonetopService extends Service {
 				if (client == null) {
 					Log.d("TCP/IPtest", "Socket is NULL");
 					return;
-				}
-				try {
-					in = client.getInputStream();
-					out = client.getOutputStream();
-					buffer = ByteBuffer.allocate(300);
-					out.write(4);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				inputHandler.open();
-				
-				while (!isEnd) {
-//					Log.d("TCP/IPtest", "before buttonEvents");
-					buttonEvents();
-				}
-
-				inputHandler.close();
+				}				
+				inputEventHandler.start();				
 			}
 		}.start();
 	}
@@ -365,14 +131,15 @@ public class PhonetopService extends Service {
 		wm.addView(view, params); // 최상위 윈도우에 뷰 넣기. 권한 필요.
 		view.setVisibility(View.INVISIBLE);
 	}
-	
+
 	public void setVisibleMouseView(int visible){
 		view.setVisibility(visible);
 	}
-	
+
 	public void onDestroy() {
 		Log.d("PhonetopService", "onDestroy()");
-		isEnd = true;
+		inputEventHandler.stop();
+//		isEnd = true;
 		if (view != null) // 서비스 종료시 뷰 제거. *중요 : 뷰를 꼭 제거 해야함.
 		{
 			((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(view);
@@ -390,7 +157,6 @@ public class PhonetopService extends Service {
 		setAddedClient(0);
 		mPhonetopDisplayManager.disconnectUsbDisplay();
 		if(MainActivity.handler!=null)MainActivity.handler.sendEmptyMessage(UIHandler.SERVICE_CLOSE);
-		Util.removeAllPreferences(this);
 		super.onDestroy();
 	}
 
@@ -400,11 +166,11 @@ public class PhonetopService extends Service {
 			MainActivity.handler.sendEmptyMessage(UIHandler.SERVICE_CONNECTED);
 		}
 	}
-	
+
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		// TODO Auto-generated method stub
-		
+
 		super.onConfigurationChanged(newConfig);
          switch(newConfig.orientation){
             case Configuration.ORIENTATION_LANDSCAPE:
@@ -414,7 +180,7 @@ public class PhonetopService extends Service {
             	view.settingOrientation(MouseView.ORIENTATION_PORTRAIT);
             	break;
          }
-        inputHandler.sendEvent(3, 0, view.getValueX());// ABS_X:0 EV_ABS:3
-		 inputHandler.sendEvent(3, 1, view.getValueY());// ABS_Y:1
+         inputEventHandler.sendEvent(3, 0, view.getValueX());// ABS_X:0 EV_ABS:3
+         inputEventHandler.sendEvent(3, 1, view.getValueY());// ABS_Y:1
 	}
 }
