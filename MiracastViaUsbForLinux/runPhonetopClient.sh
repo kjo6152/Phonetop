dbc886cab2726e111f999dc54c5c464b431bf87e
KimJiWon <kjo6152@naver.com>
2014-07-31 18:43:45 +0900

    Miracast via usb for linux 영상 재생을 제외한 부분 구현

--- MiracastViaUsbForLinux/runPhonetopClient.sh
+++ MiracastViaUsbForLinux/runPhonetopClient.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-#This sciprt is run PhoneTop client
+#This sciprt is run Phonetop client
 echo 'PhoneTop Client Running Wait...'
 
-#Following script is run PhoneTop reverse tethering
+# Step 1. If old Phonetop client is running, kill old process
+pkill -9 phonetopclient
+
+# Step 2. reverse tethering script execute
+#Following script is run Phonetop reverse tethering
 echo 'Reverse tethering running...'
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -P FORWARD DROP
@@ -11,9 +15,10 @@ iptables -A FORWARD -o usb0 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 ifconfig usb0 192.168.42.55 up
 
-#Following script is run PhoneTop client
+# Step 3. run Phonetop client
+#Following script is run Phonetop client
 #If you run via udev, you should execute to absolute path
-./main
+./phonetopclient
 
 echo 'PhoneTop Client Run !'
 
